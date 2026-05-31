bb70c18cbe407494307d3651e2eaaa8d92123d6d
Fatemeh Safaei <Ffatemehsafaeix@gmail.com>
2026-05-31 21:59:33 +0330

    annotation

---
+++ src/annotation/Test.java
@@ -0,0 +1,9 @@
+package annotation;
+
+public class Test {
+
+//    @MyTest("hi")
+    public void test() {
+        System.out.println("test");;
+    }
+}
---
+++ src/annotation/TestApplication.java
@@ -0,0 +1,18 @@
+package annotation;
+
+import java.lang.reflect.Method;
+
+public class TestApplication {
+/*    static void main() throws NoSuchMethodException {
+
+        Test test = new Test();
+        Class<?> clazz = test.getClass();
+        Method methods = clazz.getMethod("mehrana");
+
+        if (methods.isAnnotationPresent(MyTest.class)){
+            MyTest myTest = methods.getAnnotation(MyTest.class);
+            System.out.println(myTest.test());
+        }
+
+    }*/
+}
